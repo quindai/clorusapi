72bcf5314f424b3dc329634e5919e5c8d9799ce3
quindai <randy.quindai@gmail.com>
2022-02-09 12:36:23 -0300

    Integrado banco de dados Postgres rodando no docker. Integrado os bancos de dados Clorus MySQL

--- README.md
+++ README.md
@@ -4,12 +4,25 @@ API Clorus Marketeer
 
 ## Descrição do ambiente dev
 
-Iniciar o ambiente python na versão disponível, recomendo usar a versão `3.9.5`.
+Antes de iniciar, verifique se o docker e o mysql-client estão instalados na sua máquina.
+
+1. [MySQL-Client](https://pypi.org/project/mysqlclient/)
+2. [Docker](https://docs.docker.com/engine/install/)
+
+Comandos para iniciar o ambiente python na versão disponível na sua máquina, recomendo usar a versão `3.9.5`.
 
 ```python
 python3 -m venv env
 source env/bin/activate
 pip install tox
+```
+
+Verifique se o arquivo `.env` está na raiz do projeto, use como base o arquivo `.env.example`, defina o valor de todas as variavéis.
+
+Inicialize a imagem Docker do banco de dados Postgres. Se não quiser usar o docker com Postgres, basta definir DEBUG=True no arquivo `.env` para usar o sqlite. Substitua os valores da variavéis `DB_*`.
+
+```
+DB_NAME=dbname DB_USER=dbuser DB_PASS=dbpass docker-compose up
 ```
 
 Agora inicialize o ambiente com o tox. Esse comando irá instalar as dependências do projeto, fazer os migrations e realizar os testes se existirem.
--- clorusapi/settings.py
+++ clorusapi/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-n14y9icjy=_2_hef6g2v7@sp#vpa&rgts-gyxv)cervz*@1g%f'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -73,12 +74,60 @@ WSGI_APPLICATION = 'clorusapi.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
+ACTIVATED_DATABASE = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASS'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT'),
+    },
 }
+
+if DEBUG:
+    ACTIVATED_DATABASE={
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+DATABASES = {
+    config('MYSQL_DB1_NAME'): {
+        'ENGINE': 'mysql.connector.django',
+        'NAME': config('MYSQL_DB1_NAME'),
+        'USER': config('MYSQL_DB_USER'),
+        'PASSWORD': config('MYSQL_DB_PASS'),
+        'HOST': config('MYSQL_DB_HOST'),
+        'PORT': config('MYSQL_DB_PORT'),
+    },
+    config('MYSQL_DB2_NAME'): {
+        'ENGINE': 'mysql.connector.django',
+        'NAME': config('MYSQL_DB2_NAME'),
+        'USER': config('MYSQL_DB_USER'),
+        'PASSWORD': config('MYSQL_DB_PASS'),
+        'HOST': config('MYSQL_DB_HOST'),
+        'PORT': config('MYSQL_DB_PORT'),
+    },   
+    config('MYSQL_DB3_NAME'): {
+        'ENGINE': 'mysql.connector.django',
+        'NAME': config('MYSQL_DB3_NAME'),
+        'USER': config('MYSQL_DB_USER'),
+        'PASSWORD': config('MYSQL_DB_PASS'),
+        'HOST': config('MYSQL_DB_HOST'),
+        'PORT': config('MYSQL_DB_PORT'),
+    },
+    config('MYSQL_DB4_NAME'): {
+        'ENGINE': 'mysql.connector.django',
+        'NAME': config('MYSQL_DB4_NAME'),
+        'USER': config('MYSQL_DB_USER'),
+        'PASSWORD': config('MYSQL_DB_PASS'),
+        'HOST': config('MYSQL_DB_HOST'),
+        'PORT': config('MYSQL_DB_PORT'),
+    },
+}
+
+DATABASES.update(ACTIVATED_DATABASE)
 
 
 # Password validation
