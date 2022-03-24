25026a2ccb7d64cae3c2e4d407d2aa468c4c26a5
quindai <randy.quindai@gmail.com>
2022-03-24 15:30:43 -0300

    Resolvendo o comercial

--- comercial/urls.py
+++ comercial/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import ComercialAPIView
+from .views import ComercialAPIView, ComercialDetailsView
 
 urlpatterns = [
-    path('', ComercialAPIView.as_view(), name='get_comercial')
+    path('', ComercialAPIView.as_view(), name='get_comercial'),
+    path('products/', ComercialDetailsView.as_view(), name='get_products'),
 ]
--- comercial/views.py
+++ comercial/views.py
@@ -1,6 +1,10 @@
-from rest_framework import generics, mixins, permissions
+from rest_framework import generics, mixins, permissions, status
 from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.pagination import LimitOffsetPagination
 from clorusapi.permissions.basic import BasicPermission
+from accounts.models.apiuser import APIUser
+from company.models import CustomQuery
 from .models import Comercial
 from .serializers import ComercialSerializer
 
@@ -16,3 +20,18 @@ class ComercialAPIView(generics.GenericAPIView,
 
     def post(self, request, *args, **kwargs):
         return self.create(request, *args, **kwargs)
+
+class ComercialDetailsView(APIView, LimitOffsetPagination):
+    def get_object(self, user):
+        try:
+            return CustomQuery.objects.get(company=APIUser.objects.get(user=user).active_company)
+        except CustomQuery.DoesNotExist:
+            return Response({'error':'Empresa não tem query cadastrada.'},
+                        status=status.HTTP_400_BAD_REQUEST)
+
+    def get(self, request, *args, **kwargs):
+        custom_query = self.get_object(request.user)
+        query_returned = custom_query.query()
+        breakpoint()
+        response = self.paginate_queryset(query_returned, request, view=self)
+        return self.get_paginated_response(response)
