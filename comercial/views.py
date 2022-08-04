45c54d1b5a8822717ec5fd1a9d26d0fd4cf5b4ad
quindai <randy.quindai@gmail.com>
2022-08-04 17:24:05 -0300

    Comercial updated to put data

--- comercial/views.py
+++ comercial/views.py
@@ -33,12 +33,44 @@ class ComercialAPIView(generics.GenericAPIView,
         return self.create(request, *args, **kwargs)
 
     def put(self, request, *args, **kwargs):
+        """
+        Receives:
+        `{
+            "goal": {
+                "product": [
+                {
+                    "id": mandatory,
+                    "id_crm": mandatory,
+                    "name": optional,
+                    "quantity": optional,
+                    "price": optional
+                }
+                ]
+            },
+            "visualization_quant": bool -> optional,
+            "visualization_mon": bool -> optional,
+            "begin_date": date -> optional,
+            "periodicity": string -> optional,
+            "repeat_periodicity": bool -> optional,
+            "segmentation": int -> optional,
+            "id": int -> mandatory
+            }`
+        """
         if not request.data.get('id',''):
             return Response({"detail":"Insira o campo 'id'."}, status=status.HTTP_400_BAD_REQUEST)
         id = request.data.pop('id')
+        goal = request.data.pop('goal', {'product':[]})
         try:
-            # obj = Comercial.objects.filter(pk=id).update(**request.data)
             obj = Comercial.objects.filter(pk=id)
+            
+            for p in goal['product']:
+                prod_id = p.pop('id', None)
+                prod_crm=p.pop('id_crm', None)
+                if prod_id is None or prod_crm is None:
+                    return Response({
+                        "detail":f'Encontramos um problema, verifique o id "{prod_id}" do produto e o id_crm do produto "{prod_crm}"',
+                        }, status=status.HTTP_400_BAD_REQUEST)
+                obj[0].goal.product.filter(pk=prod_id, id_crm=prod_crm).update(**p)
             obj.update(**request.data)
             serializer = ComercialSerializer(obj, many=True)
         except Comercial.DoesNotExist:
